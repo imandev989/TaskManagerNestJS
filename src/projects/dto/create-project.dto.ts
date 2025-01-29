b21f4b6ca558b15b7b5fa367cde25d310f3a6400
iman.dev989 <iman.dev989@gmail.com>
2025-01-29 20:54:34 +0330

    validation

--- src/projects/dto/create-project.dto.ts
+++ src/projects/dto/create-project.dto.ts
@@ -1,6 +1,13 @@
-import ProjectStatusEnum from "../enums/projectStatusEnum";
+import ProjectStatusEnum from '../enums/projectStatusEnum';
+import { IsNotEmpty, IsString, IsEnum } from 'class-validator';
 
 export class CreateProjectDto {
-    name:string;
-    status: ProjectStatusEnum;
+  @IsNotEmpty({ message: 'name is required' })
+  @IsString({ message: 'name must be a string' })
+  name: string;
+
+  @IsEnum(ProjectStatusEnum, {
+    message: 'status must be one of the following: active, inactive, completed',
+  })
+  status: ProjectStatusEnum;
 }
