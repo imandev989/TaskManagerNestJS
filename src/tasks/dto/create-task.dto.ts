0f8e823e65576c725f3d436e6ea6557ede79be19
iman.dev989 <iman.dev989@gmail.com>
2025-01-29 22:03:21 +0330

    last modify

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,18 +1,26 @@
-import {IsString,MinLength,IsNotEmpty,IsOptional,IsEnum} from "class-validator"import TaskStatusEnum from "../enums/taskStatusEnum";
-';'
+import {
+  IsString,
+  MinLength,
+  IsNotEmpty,
+  IsOptional,
+  IsEnum,
+} from 'class-validator';
+import TaskStatusEnum from '../enums/taskStatusEnum';
 
 export class CreateTaskDto {
-    @IsString({message:"title must be String"})
-    @MinLength(3,{message:"title must be at least 3 characters"})
-    @IsNotEmpty({message:"title must be not empty"})
-    title:string;
+  @IsString({ message: 'title must be String' })
+  @MinLength(3, { message: 'title must be at least 3 characters' })
+  @IsNotEmpty({ message: 'title must be not empty' })
+  title: string;
 
-    @IsString({message:"description must be String"})
-    @MinLength(10,{message:"title must be at least 10 characters"})
-    @IsOptional()
-    description: string;
+  @IsString({ message: 'description must be String' })
+  @MinLength(10, { message: 'title must be at least 10 characters' })
+  @IsOptional()
+  description: string;
 
-    @IsEnum(TaskStatusEnum,{message:"status must be one of the following: open, in progress, done"})
-    @IsOptional()
-    status:TaskStatusEnum
+  @IsEnum(TaskStatusEnum, {
+    message: 'status must be one of the following: open, in progress, done',
+  })
+  @IsOptional()
+  status: TaskStatusEnum;
 }
