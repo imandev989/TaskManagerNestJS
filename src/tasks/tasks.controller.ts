009f340e25bfddb0b325938ab48f4430b70b04f7
iman.dev989 <iman.dev989@gmail.com>
2025-01-30 22:51:22 +0330

    implement remain Tasks

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,15 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, HttpStatus, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  HttpStatus,
+  Query,
+  Put,
+} from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
@@ -8,7 +19,7 @@ import TaskStatusEnum from './enums/taskStatusEnum';
 
 @Controller('tasks')
 export class TasksController {
-  constructor(private readonly tasksService: TasksService) { }
+  constructor(private readonly tasksService: TasksService) {}
 
   @Post()
   async create(@Body() createTaskDto: CreateTaskDto, @Res() res: Response) {
@@ -16,8 +27,8 @@ export class TasksController {
     return res.status(HttpStatus.CREATED).json({
       statusCode: HttpStatus.CREATED,
       data: newTask,
-      message: "Task Created"
-    })
+      message: 'Task Created',
+    });
   }
 
   @Get()
@@ -25,16 +36,15 @@ export class TasksController {
     @Res() res: Response,
     @Query('status') status: TaskStatusEnum,
     @Query('limit') limit: number = 10,
-    @Query('page') page: number = 1
-
+    @Query('page') page: number = 1,
   ) {
     const tasks = await this.tasksService.findAll(status, limit, page);
 
     return res.status(HttpStatus.OK).json({
       statusCode: HttpStatus.OK,
       data: tasks,
-      message: "Task Created Successfully"
-    })
+      message: 'Task Created Successfully',
+    });
   }
 
   @Get(':id')
@@ -43,17 +53,31 @@ export class TasksController {
     return res.status(HttpStatus.OK).json({
       statusCode: HttpStatus.OK,
       data: task,
-      message: "Task Recive Successfully"
-    })
+      message: 'Task Recive Successfully',
+    });
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateTaskDto: UpdateTaskDto) {
-    return this.tasksService.update(+id, updateTaskDto);
+  @Put(':id')
+  async update(
+    @Param('id') id: string,
+    @Body() createTaskDto: CreateTaskDto,
+    @Res() res: Response,
+  ) {
+    await this.tasksService.update(+id, createTaskDto);
+    return res.status(HttpStatus.OK).json({
+      statusCode: HttpStatus.OK,
+      data: null,
+      message: 'Task Updated Successfully',
+    });
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.tasksService.remove(+id);
+  async remove(@Param('id') id: string, @Res() res: Response) {
+    await this.tasksService.remove(+id);
+    return res.status(HttpStatus.OK).json({
+      statusCode: HttpStatus.OK,
+      data: null,
+      message: 'Task Removed Successflully',
+    });
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,9 @@
-import { BadRequestException, Injectable, NotFoundException, Res } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+  Res,
+} from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -13,58 +18,70 @@ export class TasksService {
     @InjectRepository(Task)
     private readonly taskRepository: Repository<Task>,
     @InjectRepository(Project)
-    private readonly projectRepository: Repository<Project>
-  ) { }
+    private readonly projectRepository: Repository<Project>,
+  ) {}
 
   async create(createTaskDto: CreateTaskDto) {
     const { projectId, ...taskData } = createTaskDto;
-    const project = await this.projectRepository.findOneByOrFail({ id: projectId })
+    const project = await this.projectRepository.findOneByOrFail({
+      id: projectId,
+    });
     try {
       const newTask = this.taskRepository.create({
         ...taskData,
-        project
+        project,
       });
-      return await this.taskRepository.save(newTask)
+      return await this.taskRepository.save(newTask);
     } catch (error) {
-      throw new BadRequestException("Erro when Task Created")
+      throw new BadRequestException('Erro when Task Created');
     }
   }
 
   async findAll(status?: TaskStatusEnum, limit: number = 10, page: number = 1) {
-
-    const query = this.taskRepository.createQueryBuilder('tasks').leftJoinAndSelect('tasks.project', 'project');
+    const query = this.taskRepository
+      .createQueryBuilder('tasks')
+      .leftJoinAndSelect('tasks.project', 'project');
     if (status) {
-      query.where('tasks.status= : status', { status })
+      query.where('tasks.status= : status', { status });
     }
-    query.skip((page - 1) * limit).take(limit)
+    query.skip((page - 1) * limit).take(limit);
     return await query.getMany();
-
   }
 
   async findOne(id: number) {
-
-    const task = await this.taskRepository.findOne({ where: { id }, relations: ['project'] });
-    if (!task) throw new NotFoundException(`Taks ${id} Not Found`)
-    return task
+    const task = await this.taskRepository.findOne({
+      where: { id },
+      relations: ['project'],
+    });
+    if (!task) throw new NotFoundException(`Taks ${id} Not Found`);
+    return task;
   }
 
   async update(id: number, updateTaskDto: UpdateTaskDto) {
+    const { projectId, ...taskData } = updateTaskDto;
     const task = await this.taskRepository.findOneBy({ id });
     if (!task) {
       throw new NotFoundException(`Taks ${id} Not Found`);
     }
+    const project = await this.projectRepository.findOneBy({ id: projectId });
+    if (!project) {
+      throw new NotFoundException(`Project ${id} Not Found`);
+    }
     try {
-      const updateTask = await this.taskRepository.update(id, updateTaskDto)
-      return updateTask
-    } catch {
-      throw new BadRequestException("updated field")
+      const updateTask = await this.taskRepository.update(id, {
+        ...taskData,
+        project,
+      });
+      return updateTask;
+    } catch (error) {
+      throw new BadRequestException('updated field', error);
     }
   }
 
   async remove(id: number) {
     const result = await this.taskRepository.delete(id);
     if (result.affected === 0) {
-      throw new NotFoundException(`task ${id} not Found`)
+      throw new NotFoundException(`task ${id} not Found`);
     }
   }
 }
